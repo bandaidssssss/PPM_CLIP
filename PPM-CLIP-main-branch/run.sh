dfbb70b2334b32e09a0d3f8a0bd394d93d619fc8
Xinyuan Wang <104727491+bandaidssssss@users.noreply.github.com>
2026-05-13 21:09:26 +0800

    Delete PPM-CLIP-main-branch directory

--- PPM-CLIP-main-branch/run.sh
+++
@@ -1,7 +0,0 @@
-python main.py --batch_size=48 --prompt_class_len=3 --lr=0.0001 --optim_type=Adam  --dataset=genimage --gpu=1   --lambda_ 0.1
-
-09-16
-python main.py --batch_size=48 --prompt_class_len=3 --lr=0.0001 --optim_type=Adam  --dataset=genimage --gpu=0   --lambda_ 0.1
-#直接ISD和repository，有IGD的基座
-
-torchrun --nproc_per_node=3 main_ddp.py --batch_size=48 --prompt_class_len=10 --lr=0.0001 --optim_type=Adam  --dataset=genimage   --lambda_ 0.1
